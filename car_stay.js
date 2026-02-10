62cc81fb0cc2a97943c045aa27b4842b503b0390
parth6020 <keshatwarparth@gmail.com>
2026-02-10 14:49:34 +0530

    Update car_stay.js

--- car_stay.js
+++ car_stay.js
@@ -1,25 +1,37 @@
-(function() {
-    let template = document.createElement("template");
-    template.innerHTML = `
-        <style>
-            :host { display: block; width: 100%; height: 100%; }
-            img { width: 100%; height: auto; display: block; }
-        </style>
-        <div id="car-container">
-            <img src="https://cdn.jsdelivr.net/gh/parth6020/Parrrth01@main/workingif.gif" alt="Car Animation">
-        </div>
-    `;
-
-    class CarStayWidget extends HTMLElement {
-        constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({ mode: "open" });
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-        }
-        connectedCallback() {
-            console.log("Widget initialized: Car will remain on screen.");
-        }
+let template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    :host {
+      display: block;
+      width: 100%;
+      height: 100%;
     }
-    // Note this tag name: car-stay-widget
-    customElements.define("car-stay-widget", CarStayWidget);
-})();
+    img {
+      width: 100%;
+      height: auto;
+    }
+  </style>
+
+  <img
+    src="https://cdn.jsdelivr.net/gh/parth6020/Parrrth01@main/workingif.gif"
+    alt="Car Animation"
+  />
+`;
+
+class CarStayWidget extends HTMLElement {
+  constructor() {
+    super();
+    if (!this.shadowRoot) {
+      const shadow = this.attachShadow({ mode: "open" });
+      shadow.appendChild(template.content.cloneNode(true));
+    }
+  }
+
+  connectedCallback() {
+    console.log("CarStayWidget loaded successfully");
+  }
+}
+
+if (!customElements.get("car-stay-widget")) {
+  customElements.define("car-stay-widget", CarStayWidget);
+}
